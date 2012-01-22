d6f855d911b93fba02846ec68e046d6958d5afc3
mathfuru <mathfuru@gmail.com>
2012-01-22 21:06:29 +0900

    うまくいく例.

--- Test.hs
+++ Test.hs
@@ -4,23 +4,18 @@ import Text.Peggy
 import Data.Maybe
 
 data Word = Word String deriving Show
-data Term = Term Word [Term] deriving Show
+data Term = Term Char [Term] deriving Show
 
 [peggy|
-nums :: [Int]
-  = num*
-num ::: Int
-  = [0-9]+ { read $1 }
-word ::: Word
-  = [a-z] { Word [$1] }
-term :: Term
-  = word ("(" term* ")")? { Term $1 $ fromMaybe [] $2 }
+term ::: (Char, Maybe String) = word ('(' (digit_)* ')')? { ($1, $2) }  -- a(b;cd();)
+digit_ :: Char = [0-9]
+word :: Char = [a-z]
 |]
 
 main :: IO ()
 main = do
-  print $ parseString word "" "a"
-  print $ parseString term "" "a"
+  --print $ parseString word "" "a"
+  --print $ parseString words_ "" "a"
   print $ parseString term "" "a()"
-  print $ parseString term "" "a(b)"
-  print $ parseString term "" "a(bc)"
+  print $ parseString term "" "a(3)"
+  print $ parseString term "" "a(981)"
