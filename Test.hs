115de1b2763cb53ed4608435a97a29622b5a2617
mathfuru <mathfuru@gmail.com>
2012-01-27 08:40:39 +0900

    a(h;o;) =>      array a(ho);  =>      array(hash, object) a;"isA";        =>      array

--- Test.hs
+++ Test.hs
@@ -6,26 +6,36 @@ import Data.List
 import Control.Monad.State
 import Data.Functor.Identity
 
-data Word = Word Char deriving Show
-data Term = Term Word [Term] deriving Show
+data Prefix = Prefix String deriving Show
+data Term = Term Prefix [Term] deriving Show
 
+-- TODO: 
+-- * なぜtermsが途中までのヒットでもマッチしてしまうか調べる
+-- * a"isA"(b;c;)の展開
+-- * e(.e)の展開
 [peggy|
-term ::: Term = word ('(' terms ')')? { Term (Word $1) (fromMaybe [] $2) }  -- a(b;cd();)
-terms ::: [Term] = (term ';')+
-word :: Char = [a-z]
+term :: Term = prefix ('(' terms ')')? { Term $1 (fromMaybe [] $2) }  -- a(b;cd();)
+terms :: [Term] = term+
+                / (term ';')+
+prefix :: Prefix = '\"' abbr_word '\"' { Prefix $1 }
+                 / [a-z] { Prefix [$1] }
+abbr_word :: String = [a-zA-Z0-9]+
 |]
 
 toString :: String -> [String] -> String
-toString name args = name ++ "(" ++ (intercalate ";" args) ++ ")"
+toString name args = name ++ (if (length args == 0) then "" else ("(" ++ (intercalate "," args) ++ ")"))
 
 expandTerm :: Term -> StateT [(Char, String)] Identity String
-expandTerm (Term (Word c) ts) = do
-  source <- gets ((fromMaybe [c] ).(lookup c))
+expandTerm (Term (Prefix cs) ts) = do
+  source <- gets ((fromMaybe [head cs] ).(lookup $ head cs))
   xs <- (mapM expandTerm ts)
   return $ toString source xs
 
 main = do
   let environment = [('f', "function"), ('h', "hash"), ('a', "array"), ('o', "object")] 
-  case (parseString term "" "a(h;o;)") of
-    Right t -> print $ evalState (expandTerm t) environment
-    Left e -> print e
+  let samples =  ["a(h;o;)", "a(ho);", "a;\"isA\";"]
+  forM_ samples (\sample ->
+    case (parseString terms "" sample) of
+      Right ts -> putStrLn ( sample ++ "\t=>\t" ++ (intercalate "\t" (evalState (mapM expandTerm ts) environment )))
+      Left e -> print e
+    )
