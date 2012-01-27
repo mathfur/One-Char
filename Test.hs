6360d5aa6e186a9124c958160d71e6fac111b20f
mathfuru <mathfuru@gmail.com>
2012-01-27 09:23:13 +0900

    セミコロン有りのtermsにマッチしない問題を修正.

--- Test.hs
+++ Test.hs
@@ -6,19 +6,20 @@ import Data.List
 import Control.Monad.State
 import Data.Functor.Identity
 
-data Prefix = Prefix String deriving Show
+data Prefix = OneCharPrefix Char
+            | SubSeqPrefix String
+            deriving Show
 data Term = Term Prefix [Term] deriving Show
 
--- TODO: 
--- * なぜtermsが途中までのヒットでもマッチしてしまうか調べる
+-- TODO:
 -- * a"isA"(b;c;)の展開
 -- * e(.e)の展開
 [peggy|
 term :: Term = prefix ('(' terms ')')? { Term $1 (fromMaybe [] $2) }  -- a(b;cd();)
-terms :: [Term] = term+
-                / (term ';')+
-prefix :: Prefix = '\"' abbr_word '\"' { Prefix $1 }
-                 / [a-z] { Prefix [$1] }
+terms :: [Term] = (term ';')+
+                / term+
+prefix :: Prefix = '\"' abbr_word '\"' { SubSeqPrefix $1 }
+                 / [a-z] { OneCharPrefix $1 }
 abbr_word :: String = [a-zA-Z0-9]+
 |]
 
@@ -26,13 +27,17 @@ toString :: String -> [String] -> String
 toString name args = name ++ (if (length args == 0) then "" else ("(" ++ (intercalate "," args) ++ ")"))
 
 expandTerm :: Term -> StateT [(Char, String)] Identity String
-expandTerm (Term (Prefix cs) ts) = do
-  source <- gets ((fromMaybe [head cs] ).(lookup $ head cs))
+expandTerm (Term prefix ts) = do
+  source <- gets ((fromMaybe $ [headerChar prefix] ).(lookup $ headerChar prefix))
   xs <- (mapM expandTerm ts)
   return $ toString source xs
 
+headerChar :: Prefix -> Char
+headerChar (OneCharPrefix c) = c
+headerChar (SubSeqPrefix cs) = head cs
+
 main = do
-  let environment = [('f', "function"), ('h', "hash"), ('a', "array"), ('o', "object")] 
+  let environment = [('f', "function"), ('h', "hash"), ('a', "array"), ('o', "object")]
   let samples =  ["a(h;o;)", "a(ho);", "a;\"isA\";"]
   forM_ samples (\sample ->
     case (parseString terms "" sample) of
